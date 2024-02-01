d4dd8ccb659f67426d9dd42eef0c30b773180507
PRATYAKSH9923 <81825663+PRATYAKSH9923@users.noreply.github.com>
2024-02-01 23:40:54 +0530

    workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust the limit to your desired value
+  },
   plugins: [react()]
   // base: "/NetflixGPT/"
 })
